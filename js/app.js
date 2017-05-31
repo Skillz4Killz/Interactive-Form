81313cb89695c476523384f31c0d96389ec463ac
Skillz <admin@ezlgg.com>
2017-05-30 20:16:59 -0400

    job role step completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 //declare all global variables
-
 const name = document.getElementById('name');
+const jobRole = document.getElementById('title');
+const otherRole = document.getElementById('other-title');
 
 name.focus();
+otherRole.style.display = 'none';
+
+jobRole.addEventListener('change', () => {
+    if (jobRole.value == 'other') {
+      otherRole.style.display = '';
+    } else if (jobRole.value != 'other') {
+        otherRole.style.display = 'none';
+    }
+})
+
+

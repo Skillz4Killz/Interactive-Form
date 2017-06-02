3a509e110cf77cce23154796d3f338bb6b7c7f20
Skillz4Killz <admin@ezlgg.com>
2017-06-02 15:51:07 -0400

    Completed project

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 //declare all global variables
 const name = document.getElementById('name');
+const emailInput = document.getElementById('mail');
 const jobRole = document.getElementById('title');
 const otherRole = document.getElementById('other-title');
 const shirtDesign = document.getElementById('design');
 const shirtColor = document.getElementById('color');
+const colorPuns = document.getElementById('colors-js-puns');
 let subTotal = 0;
 let insertCost = 'Total Cost = ';
 const allActivities = document.getElementsByClassName('activities');
+let numOfActivities = 0;
 const mainConf = document.getElementsByName('all')[0];
 const jsFrameworks = document.getElementsByName('js-frameworks')[0];
 const jsLibs = document.getElementsByName('js-libs')[0];
@@ -23,10 +26,14 @@ const email = document.getElementById('mail');
 const creditCard = document.getElementById('credit-card');
 const paypal = document.getElementsByTagName('p')[0];
 const bitcoin = document.getElementsByTagName('p')[1];
+
+//Declare error variables with a paragraph element and also assign them a errorClass.
 const errorName = document.createElement('p');
 errorName.className = 'errorClass';
 const errorEmail = document.createElement('p');
 errorEmail.className = 'errorClass';
+const errorOtherRole = document.createElement('p');
+errorOtherRole.className = 'errorClass';
 const errorDesign = document.createElement('p');
 errorDesign.className = 'errorClass';
 const errorActivities = document.createElement('p');
@@ -40,12 +47,12 @@ errorZipCode.className = 'errorClass';
 const errorCvv = document.createElement('p');
 errorCvv.className = 'errorClass';
 
-//Preparing Form on page load.s
+//Preparing Form on page load. Focus on name, hide the otherRole, hide the colors selector, hide paypal and bitcoin.
 name.focus();
 otherRole.style.display = 'none';
-document.getElementById('colors-js-puns').style.display = 'none';
-document.getElementsByTagName('p')[0].style.display = 'none';
-document.getElementsByTagName('p')[1].style.display = 'none';
+colorPuns.style.display = 'none';
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
 
 //showing Other Text field only when Other is selected
 jobRole.addEventListener('change', () => {
@@ -60,21 +67,22 @@ jobRole.addEventListener('change', () => {
 shirtDesign.addEventListener('change', () => {
     if (shirtDesign.value === 'js puns') {
     shirtColor.innerHTML = '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option><option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> <option value="gold">Gold (JS Puns shirt only)</option>';
-    document.getElementById('colors-js-puns').style.display = '';
+    colorPuns.style.display = '';
 } else if (shirtDesign.value === 'heart js') {
     shirtColor.innerHTML = '<option value="tomato">Tomato (I &#9829; JS shirt only)</option><option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>';
-    document.getElementById('colors-js-puns').style.display = '';
+    colorPuns.style.display = '';
 } else {
-    document.getElementById('colors-js-puns').style.display = 'none';
+    colorPuns.style.display = 'none';
 }
 })
 
 //Totaling Costs and scheduling properly.
  totalCost = document.createElement('p');
     totalCost.className = 'totalCost';
+    //inserts the element but not content
     npm.parentElement.after(totalCost);
-let numOfActivities = 0;
 
+//If mainConf is checked add 200 to the cost and if unchecked remove 200 from cost
 function mainActivity () {
     mainConf.addEventListener('change', () => {
         if (mainConf.checked) {
@@ -89,7 +97,7 @@ function mainActivity () {
     })
 }
 
-
+//if any other conference is checked add 100 or remove 100 if unchecked.
 function activities (x) {
     x.addEventListener('change', () => {
         if (x.checked) {
@@ -101,6 +109,7 @@ function activities (x) {
             totalCost.textContent = insertCost + subTotal;
             numOfActivities++;
         }
+// hide all conferences that overlap on time.
         if (jsFrameworks.checked) {
             express.parentElement.style.display = 'none';
             buildTools.parentElement.style.display = 'none';
@@ -115,7 +124,6 @@ function activities (x) {
             jsFrameworks.parentElement.style.display = '';
             express.parentElement.style.display = '';
         }
-
         if (jsLibs.checked) {
             node.parentElement.style.display = 'none';
             npm.parentElement.style.display = 'none';
@@ -129,12 +137,11 @@ function activities (x) {
             npm.parentElement.style.display = '';
             jsLibs.parentElement.style.display = '';
             node.parentElement.style.display = '';
-        }
-        
+        } 
     })
-    
 }
 
+//call previous function for each type of activity.
 mainActivity(mainConf);
 activities(jsFrameworks);
 activities(jsLibs);
@@ -143,7 +150,7 @@ activities(node);
 activities(buildTools);
 activities(npm);
 
-//Payment selecting changing options per option.
+// Hide or show bitcoin/paypal/credit card based on option chosen.
 paymentType.addEventListener('change', () => {
     if (paymentType.value === 'bitcoin') {
         creditCard.style.display = 'none';
@@ -160,32 +167,41 @@ paymentType.addEventListener('change', () => {
         bitcoin.style.display = 'none';
     }
 })
+
+//function to remove all errors once the proper input is made
 function removePaymentErrors () {
     errorPayment.remove();
     errorCreditCard.remove();
     errorZipCode.remove();
     errorCvv.remove();
 }
-//Form Validation
+
+//Form Validation placed on the submit button.
 button.addEventListener('click', () => {
+// if any blank inputs run the code
     if (name.value == '' || email.value === '' || otherRole === '' || paymentType.value === 'credit-card' || 
     paymentType.value === 'select-method' || numOfActivities === 0) {
-        if (name.value == '' || email.value === '' ) {
+// if blank name or email run stop the submit and throw errors on form.
+        if (name.value == '') {
             event.preventDefault();
-            if (name.value == '') {
-                errorName.textContent = 'Please make sure to add in your name.';
-                document.getElementById('name').after(errorName);
-                document.getElementById('name').style.borderColor = 'red';
-            } 
-            if (email.value == '') {
-                errorEmail.textContent = 'Please make sure to add in your email address.';
-                document.getElementById('mail').after(errorEmail);
-                document.getElementById('mail').style.borderColor = 'red';
-            } 
-    }
+            errorName.textContent = 'Please make sure to add in your name.';
+            name.after(errorName);
+            name.style.borderColor = 'red';
+        } 
+        if (email.value == '') {
+            event.preventDefault();
+            errorEmail.textContent = 'Please make sure to add in your email address.';
+            emailInput.after(errorEmail);
+            emailInput.style.borderColor = 'red';
+        } 
+// if jobRole is other and the textField is blank stop the submit and throw errors on form.
         if (title.value === 'other' && otherRole === '') {
             event.preventDefault();
+            errorOtherRole.textContent = 'Please make sure to add in your role.'
+            otherRole.after(errorOtherRole);
+            otherRole.style.borderColor = 'red';
         }
+// if a design is not selected stop the submit and throw errors on form.
         if (document.getElementById('design').value === 'Select Theme') {
             errorDesign.textContent = 'Please make sure to select a design type';
             document.getElementById('design').style.borderColor = 'red';
@@ -193,50 +209,50 @@ button.addEventListener('click', () => {
             document.getElementById('design').after(errorDesign);
             event.preventDefault();
         }
-        
+// if there are 0 activities checked stop the submit and throw errors on form.
         if (numOfActivities === 0) {
             errorActivities.textContent = 'Please make sure to select atleast one activity.';
                 $('.activities').css('color', 'red');
                 $('.activities').after(errorActivities);
                 event.preventDefault();
         }
-
-        if (paymentType.value === 'credit-card' || paymentType.value === 'select-method' || payment.value === 'select_method') {
-            
-            if (document.getElementById('cc-num').value === '' || document.getElementById('cc-num').value.length < 13 || document.getElementById('cc-num').value.length > 16) {
-                errorCreditCard.textContent = 'Please make sure to add in your Credit Card Number containing 13-16 digits.';
-                $('label[for="cc-num"]').css('color', 'red');
-                document.getElementById('cc-num').style.borderColor = 'red';
-                document.getElementById('payment').after(errorCreditCard);
-                event.preventDefault();
-            }
-            if (document.getElementById('zip').value === '' || document.getElementById('zip').value.length != 5) {
-                errorZipCode.textContent = 'Please make sure to add in your 5-digit Zip Code.';
-                document.getElementById('zip').style.borderColor = 'red';
-                $('label[for="zip"]').css('color', 'red');
-                document.getElementById('payment').after(errorZipCode);
-                event.preventDefault();
-            }
-            if (document.getElementById('cvv').value === '' || document.getElementById('cvv').value.length != 3) {
-                errorCvv.textContent = 'Please make sure to add in your 3-digit CVV number.';
-                document.getElementById('cvv').style.borderColor = 'red';
-                $('label[for="cvv"]').css('color', 'red');
-                document.getElementById('payment').after(errorCvv);
-                event.preventDefault();
-            }
-            if (payment.value === 'select_method') {
-                errorPayment.textContent = 'Please make sure to select a payment option.';
-                document.getElementById('payment').style.borderColor = 'red';
-                document.getElementById('payment').after(errorPayment);
-                event.preventDefault();
-            }
+// if CreditCard# is blank or not 13-16 digits stop submit and throw errors       
+        if (document.getElementById('cc-num').value === '' || document.getElementById('cc-num').value.length < 13 || document.getElementById('cc-num').value.length > 16) {
+            errorCreditCard.textContent = 'Please make sure to add in your Credit Card Number containing 13-16 digits.';
+            $('label[for="cc-num"]').css('color', 'red');
+            document.getElementById('cc-num').style.borderColor = 'red';
+            document.getElementById('payment').after(errorCreditCard);
+            event.preventDefault();
         }
-        
+// if ZipCode is blank or not 5 digits stop submit and throw errors
+        if (document.getElementById('zip').value === '' || document.getElementById('zip').value.length != 5) {
+            errorZipCode.textContent = 'Please make sure to add in your 5-digit Zip Code.';
+            document.getElementById('zip').style.borderColor = 'red';
+            $('label[for="zip"]').css('color', 'red');
+            document.getElementById('payment').after(errorZipCode);
+            event.preventDefault();
+        }
+// if CVV is blank or not 3 digits stop submit and throw errors
+        if (document.getElementById('cvv').value === '' || document.getElementById('cvv').value.length != 3) {
+            errorCvv.textContent = 'Please make sure to add in your 3-digit CVV number.';
+            document.getElementById('cvv').style.borderColor = 'red';
+            $('label[for="cvv"]').css('color', 'red');
+            document.getElementById('payment').after(errorCvv);
+            event.preventDefault();
+        }
+// if Payment type is not selected stop submit and throw errors
+        if (payment.value === 'select_method') {
+            errorPayment.textContent = 'Please make sure to select a payment option.';
+            document.getElementById('payment').style.borderColor = 'red';
+            document.getElementById('payment').after(errorPayment);
+            event.preventDefault();
+        }
     }
+//assign the red color to the errorClass that appear on form
     $('.errorClass').css('color', 'red');
 })
 
-
+//When name has an error and is now fixed remove errors
 name.addEventListener('keypress', () => {
     if (name.value != '') {
         document.getElementById('name').style.borderColor = '';
@@ -244,6 +260,7 @@ name.addEventListener('keypress', () => {
     }
 })
 
+//When email has an error and is now fixed remove errors
 email.addEventListener('keypress', () => {
     let tempEmail = email.value;
     if (tempEmail.search('@') === -1) {
@@ -256,6 +273,7 @@ email.addEventListener('keypress', () => {
     }
 })
 
+//When Design has an error and is now fixed remove errors
 shirtDesign.addEventListener('change', () => {
     if (shirtDesign.value != '') {
         document.getElementById('design').style.borderColor = '';
@@ -263,48 +281,56 @@ shirtDesign.addEventListener('change', () => {
     }
 })
 
+//When mainConf is checked remove the activities errors.
 mainConf.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When jsFrameworks is checked remove the activities errors.
 jsFrameworks.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When jsLibs is checked remove the activities errors.
 jsLibs.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When express is checked remove the activities errors.
 express.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When node is checked remove the activities errors.
 node.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When buildtools is checked remove the activities errors.
 buildTools.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When npm is checked remove the activities errors.
 npm.addEventListener('change', () => {
     if (numOfActivities > 0) {
         $('.activities').css('color', '');
         errorActivities.remove();  
     }
 })
+//When paymentType has errors and then changed to a different type either paypal or bitcoin remove errors.
 paymentType.addEventListener('change', () => {
     if (paymentType.value != 'select_method') {
         document.getElementById('payment').style.borderColor = '';
@@ -319,6 +345,7 @@ paymentType.addEventListener('change', () => {
         removePaymentErrors();
     }
 })
+//When ccNum is not correct throw error and if it is correct remove errors.
 ccNum.addEventListener('change', () => {
     if (ccNum.value.length >= 13 && ccNum.value.length <= 16) {
         document.getElementById('cc-num').style.borderColor = '';
@@ -331,6 +358,7 @@ ccNum.addEventListener('change', () => {
         document.getElementById('payment').after(errorCreditCard);
     }
 })
+//When zipcode is not correct throw error and if it is correct remove errors.
 zipCode.addEventListener('change', () => {
     if (zipCode.value.length == 5) {
         document.getElementById('zip').style.borderColor = '';
@@ -345,6 +373,7 @@ zipCode.addEventListener('change', () => {
 
     }
 })
+//When CVV is not correct throw error and if it is correct remove errors.
 cvvNum.addEventListener('change', () => {
     if (cvvNum.value.length == 3) {
         document.getElementById('cvv').style.borderColor = '';
@@ -357,4 +386,3 @@ cvvNum.addEventListener('change', () => {
         document.getElementById('payment').after(errorCvv);
     }
 })
-
